dfb0310dd9891c37f38d16b56182a3463f0f0a8c
Richard Marshall <rich.qazwsx@gmail.com>
2024-01-09 22:32:16 -0800

    login and signup setup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -23,13 +23,35 @@ export default function SignupPage() {
       <hr />
       <label htmlFor="username">username</label>
       <input 
-        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="username" 
         type="text" 
         value={user.username}
         onChange={(e) => setUser({...user, username: e.target.value})}
         placeholder="username"
       />
+      <label htmlFor="email">email</label>
+      <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email" 
+        type="email" 
+        value={user.email}
+        onChange={(e) => setUser({...user, email: e.target.value})}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password" 
+        type="password" 
+        value={user.password}
+        onChange={(e) => setUser({...user, password: e.target.value})}
+        placeholder="password"
+      />
+      <button 
+      onClick={onSignup}
+      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup here!</button>
+      <Link href="/login">Visit Login page</Link>
     </div>
   )
 }
