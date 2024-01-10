f793b5ccebb326325f4723903889ff20f2599e7f
Richard Marshall <rich.qazwsx@gmail.com>
2024-01-09 21:32:14 -0800

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center h-100">
+      <h1 className="text-white text-2xl">Login</h1>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  })
+
+  const onSignup = async () => {
+
+  }
+
+  return (
+    <div className="flex flex-col items-center
+    justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input 
+        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="username" 
+        type="text" 
+        value={user.username}
+        onChange={(e) => setUser({...user, username: e.target.value})}
+        placeholder="username"
+      />
+    </div>
+  )
+}
